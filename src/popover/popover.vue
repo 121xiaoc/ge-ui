9eafc3fd1005f6e229208d563e5f30936270141b
121xiaoc <1218969916@qq.com>
2019-06-05 20:48:12 +0800

    'popover位置设置以及小三角的设置'

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -2,10 +2,14 @@
     <span class="g-popover-box">
         <transition 
             name='fade-in-linear'>
-            <div class="g-popover" 
+            <div class="g-popover"
+                :class="[
+                    placement ? 'g-popover-' + placement: ''
+                ]" 
                 v-show="showPopover"
                 ref="popover">
-                popover
+                popoverpopoverpopoverpopoverpopoverpopoverpopover
+                <div class="popover-arrow"></div>
             </div>
         </transition>
         <slot name="reference"></slot>
@@ -13,7 +17,7 @@
 </template>
 
 <script>
-import { addClass, on } from '../utils/dom.js'
+import { on } from '../utils/dom.js'
 export default {
     name: 'g-popover',
     data () {
@@ -26,6 +30,11 @@ export default {
             type: String,
             default: 'click',
             validator: value => ['click', 'hover'].includes(value)
+        },
+        placement: {
+            type: String,
+            default: 'top',
+            validator: value => ['top', 'bottom', 'left', 'right'].includes(value)
         }
     },
     watch: {
@@ -59,7 +68,8 @@ export default {
         // console.log(reference)
         if (reference) { // 判断 reference 插槽是否有值插入
             // console.log(hasClass(reference, 'g-button-success'))
-            addClass(reference, 'list')
+            // addClass(reference, 'list')
+            console.log(this.trigger)
             if (this.trigger === 'click') {
                 on(reference, 'click', this.onClickToggle)
             }
@@ -78,6 +88,7 @@ export default {
 @import '../common/css/transitions';
 $color-background-popover: #ffffff;
 $color-border-popover: #ebeef5;
+$space-popover: 10px;
 .g-popover-box {
     position: relative;
     display: inline-block;
@@ -85,13 +96,71 @@ $color-border-popover: #ebeef5;
 
 .g-popover {
     position: absolute;
-    bottom: 120%;
+    display: inline-block;
     width: 200px;
     box-shadow: $box-shadow;
     padding: 10px 14px;
     border: 1px solid $color-border-popover;
     border-radius: 4px;
     background: $color-background-popover;
+    word-break: break-all;
+    &.g-popover-top {
+        bottom: 100%;
+        margin-bottom: $space-popover;
+        .popover-arrow {
+            top: 100%;
+            border-top-color: #fff;
+        }
+    }
+    &.g-popover-bottom {
+        top: 100%;
+        margin-top: $space-popover;
+        .popover-arrow {
+            bottom: 100%;
+            border-bottom-color: #fff;
+        }
+    }
+    &.g-popover-top, &.g-popover-bottom {
+        left: 50%;
+        transform: translateX(-50%);
+        .popover-arrow {
+            left: 50%;
+            transform: translateX(-50%);
+        }
+    }
+    &.g-popover-left {
+        right: 100%;
+        margin-right: $space-popover;
+        .popover-arrow {
+            left: 100%;
+            border-left-color: #fff;
+        }
+    }
+    &.g-popover-right {
+        left: 100%;
+        margin-left: $space-popover;
+        .popover-arrow {
+            right: 100%;
+            border-right-color: #fff;
+        }
+    }
+    &.g-popover-left, &.g-popover-right {
+        top: 50%;
+        transform: translateY(-50%);
+        .popover-arrow {
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+    .popover-arrow {
+        position: absolute;
+        width: 0;
+        height: 0;
+        border-width: 6px;
+        border-style: solid;
+        border-color: transparent;
+        filter: drop-shadow(0 3px 12px rgba(0,0,0,.1)) 
+    }
 }
 
 
